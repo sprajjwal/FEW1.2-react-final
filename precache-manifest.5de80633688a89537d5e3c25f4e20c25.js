b72db612ef182d4cfcd50bac1d85e69dd29c73a3
sprajjwal <shashwatprajjwal@gmail.com>
2020-03-04 16:58:50 -0800

    Updates

---
+++ precache-manifest.5de80633688a89537d5e3c25f4e20c25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6365ae30ff27067501aa6fdf119ebe2",
+    "url": "/FEW1.2-react-final/index.html"
+  },
+  {
+    "revision": "f87970eb2e6906154642",
+    "url": "/FEW1.2-react-final/static/css/main.5cab0cd0.chunk.css"
+  },
+  {
+    "revision": "657459463560537d2d7f",
+    "url": "/FEW1.2-react-final/static/js/2.4fb1b171.chunk.js"
+  },
+  {
+    "revision": "4e47723aef56c4c7d3a45ec806795252",
+    "url": "/FEW1.2-react-final/static/js/2.4fb1b171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f87970eb2e6906154642",
+    "url": "/FEW1.2-react-final/static/js/main.3b9042bd.chunk.js"
+  },
+  {
+    "revision": "e2471c07353782d9a52e",
+    "url": "/FEW1.2-react-final/static/js/runtime-main.00e96cc1.js"
+  }
+]);
